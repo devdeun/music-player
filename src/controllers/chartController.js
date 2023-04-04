a350393548ab43d412b8c039d75275533742fc29
deun <devdeun@gmail.com>
2023-04-04 21:32:23 +0900

    fix: sort error

--- src/controllers/chartController.js
+++ src/controllers/chartController.js
@@ -2,7 +2,9 @@ import Chart from "../models/Chart";
 
 export const top100chart = async (req, res) => {
   try {
-    const charts = await Chart.find();
+    const charts = await Chart.find().sort(
+      (a, b) => Number(a.rank) - Number(b.rank)
+    );
     return res.render("top100", { pageTitle: "Top100", charts });
   } catch (err) {
     console.error(err);
