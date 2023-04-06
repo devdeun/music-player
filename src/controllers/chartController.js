6466b92ab96aec502d37f0403cd694a08d441439
deun <devdeun@gmail.com>
2023-04-07 01:23:06 +0900

    fix: Chart sort error

--- src/controllers/chartController.js
+++ src/controllers/chartController.js
@@ -2,10 +2,9 @@ import Chart from "../models/Chart";
 
 export const top100chart = async (req, res) => {
   try {
-    const charts = await Chart.find().sort(
-      (a, b) => Number(a.rank) - Number(b.rank)
-    );
-    return res.render("top100", { pageTitle: "Top100", charts });
+    const charts = await Chart.find();
+    const sortedCharts = charts.sort((a, b) => Number(a.rank) - Number(b.rank));
+    return res.render("top100", { pageTitle: "Top100", charts: sortedCharts });
   } catch (err) {
     console.error(err);
   }
--- src/routers/chartRouter.js
+++ src/routers/chartRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { top100chart } from "../controllers/chartController";
 
 const chartRouter = express.Router();
+
 chartRouter.get("/", top100chart);
 
 export default chartRouter;
