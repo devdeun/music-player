f2ad946c82868df4fdfaaf7449f2e6afd04f1d90
deun <devdeun@gmail.com>
2023-04-09 00:52:32 +0900

    feat: Implement getting video from youtube API based on URL

--- src/client/js/musicPlayer.js
+++ src/client/js/musicPlayer.js
@@ -1,3 +1,5 @@
+import YoutubePlayer from "./components/YoutubePlayer";
+
 const getSearchResultFromUrl = async searchValue => {
   return await fetch(`/youtubeUrl?url=${searchValue}`, {
     method: "GET",
@@ -5,8 +7,7 @@ const getSearchResultFromUrl = async searchValue => {
     .then(response => response.json())
     .catch(err => console.error(err));
 };
-
-const renderSearchResult = (title, channelTitle, thumbnail) => {
+const renderSearchResult = ({ title, channelTitle, thumbnail }) => {
   const $searchResult = document.querySelector(".search-result");
   $searchResult.innerHTML = `
     <h3>${title} by ${channelTitle}</h3>
@@ -14,18 +15,42 @@ const renderSearchResult = (title, channelTitle, thumbnail) => {
   `;
 };
 
+const renderPlaylist = ({ id, title, channelTitle, thumbnail }) => {
+  const $playlist = document.querySelector(".music-playlist");
+  const $li = document.createElement("li");
+  $li.dataset.id = id;
+  $li.innerHTML = `
+    <h3>${title} by ${channelTitle}</h3>
+    <img src="${thumbnail}" alt="" />
+  `;
+  $playlist.appendChild($li);
+};
+
 const handleSearchSubmit = async event => {
   event.preventDefault();
   const $searchInput = document.querySelector(".search-input");
   const searchValue = $searchInput.value;
   $searchInput.value = "";
-  const { title, channelTitle, thumbnail } = await getSearchResultFromUrl(
-    searchValue
-  );
-  renderSearchResult(title, channelTitle, thumbnail);
+  const result = await getSearchResultFromUrl(searchValue);
+  renderSearchResult(result);
+  renderPlaylist(result);
+};
+
+const handlePlaylistClick = async (event, youtubePlayer) => {
+  const $li = event.target.closest("li");
+  const videoId = $li.dataset.id;
+  if (!youtubePlayer.player) youtubePlayer.setPlayer();
+  youtubePlayer.setVideoId(videoId);
 };
 
 export const initMusicPlayer = () => {
   const $searchForm = document.querySelector("#search-form");
+  const $playlist = document.querySelector(".music-playlist");
+  const youtubePlayer = new YoutubePlayer();
+  youtubePlayer.loadIframeApi();
+
   $searchForm.addEventListener("submit", handleSearchSubmit);
+  $playlist.addEventListener("click", event =>
+    handlePlaylistClick(event, youtubePlayer)
+  );
 };
